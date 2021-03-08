efc0a6e0750e08f387358d4fc1097f8e2d1e96ae
dinglixiao <1713716789@qq.com>
2021-03-08 09:34:24 +0800

    file changes

--- src/std/numeric.cpp
+++ src/std/numeric.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <numeric>
+#include <thread>
 
 using namespace std;
 
+void myprint() {
+    cout << "hello1" << endl;
+    cout << "hello2" << endl;
+    cout << "hello3" << endl;
+    cout << "hello4" << endl;
+    cout << "hello5" << endl;
+    cout << "hello6" << endl;
+}
+
 int main() {
 
-    cout << "nullptr" << endl;
+    std::count_if()
+    return 0;
 }
