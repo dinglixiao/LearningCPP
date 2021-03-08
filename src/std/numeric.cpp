3be293057b365187a7ded2bf9f9a255c03329a94
dinglixiao <1713716789@qq.com>
2021-03-08 09:34:39 +0800

    file changes

--- src/std/numeric.cpp
+++ src/std/numeric.cpp
@@ -4,17 +4,8 @@
 
 using namespace std;
 
-void myprint() {
-    cout << "hello1" << endl;
-    cout << "hello2" << endl;
-    cout << "hello3" << endl;
-    cout << "hello4" << endl;
-    cout << "hello5" << endl;
-    cout << "hello6" << endl;
-}
 
 int main() {
 
-    std::count_if()
     return 0;
 }
